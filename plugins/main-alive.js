0f92b5939d1fe8920f7ecb66d42afe67cb42ca39
➳Chethiya_MD╰‿╯穴༺ <chethiyamd@gmail.com>
2025-05-29 13:39:52 +0530

    Add files via upload

---
+++ plugins/main-alive.js
@@ -0,0 +1,49 @@
+const { cmd } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const config = require('../config');
+
+cmd({
+    pattern: "alive",
+    alias: ["status", "online", "a"],
+    desc: "Check bot is alive or not",
+    category: "main",
+    react: "⚡",
+    filename: __filename
+},
+async (conn, mek, m, { from, sender, reply }) => {
+    try {
+        const status = `
+╭───〔 *🤖 KAVIYA-MD STATUS* 〕───◉
+│✨ *Bot is Active & Online!*
+│
+│🧠 *Owner:* KAVIYA-MD
+│⚡ *Version:* 0.0.1
+│📝 *Prefix:* [.]
+│📳 *Mode:* [${config.MODE}]
+│💾 *RAM:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
+│🖥️ *Host:* ${os.hostname()}
+│⌛ *Uptime:* ${runtime(process.uptime())}
+╰────────────────────◉
+> KAVIYA-MD`;
+
+        await conn.sendMessage(from, {
+            image: { url:"https://i.ibb.co/201XgZ5f/SulaMd.jpg" },
+            caption: status,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 1000,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363399450741861@newsletter',
+                    newsletterName: 'KAVIYA-MD',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error("Alive Error:", e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
